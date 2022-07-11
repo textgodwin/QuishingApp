67da18cc30ccc54907c17badf695d87f1d2ce45c
textgodwin <textgodwin@gmail.com>
2022-07-11 18:18:46 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+import joblib
+import uvicorn
+
+import pickle
+from models import Website
+
+app = FastAPI()
+# pkl
+phish_model = open('phishing.pkl', 'rb')
+phish_model_ls = joblib.load(phish_model)
+
+
+@app.get("/{name}")
+def hello(name):
+    return {"Hello {} and Welcome to this API".format(name)}
+
+
+@app.get("/")
+def greet():
+    return {"Hello World"}
+
+
+# ML Aspect
+@app.post('/predict')
+async def predict(reg: Website):
+    feat = [reg.url]
+    feature = feat
+
+    y_Predict = phish_model_ls.predict(feature)
+    probab = phish_model_ls.predict(feature)
+
+    if(y_Predict==1):
+        return {"Class": "{This is a phishing site}"}
+    else:
+        return {"Class": "{This is a legitimate site}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
