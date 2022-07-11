f1f6379ce96deb20baad807d539c684442f02c10
textgodwin <textgodwin@gmail.com>
2022-07-11 23:19:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,22 +15,22 @@ phish_model_ls = joblib.load(phish_model)
 def greet():
     return {"Hello World"}
 
+app = FastAPI()
+#pkl
+phish_model = open('phishing.pkl', 'rb')
+phish_model_ls = joblib.load(phish_model)
 
-# ML Aspect
-@app.post('/predict')
-async def predict(reg: Website):
-    feat = [reg.url]
-    feature = feat
-
-    y_Predict = phish_model_ls.predict(feature)
-    probab = phish_model_ls.predict(feature)
-
-    if(y_Predict==1):
-        return "{This is a phishing site}"
-        
-    else:
-        return "{This is a legitimate site}"
-      
+#ML Aspect
+@app.get('/predict')
+async def predict(features):
+	url = []
+	url.append(str(features))
+	y_Predict = phish_model_ls.predict(url)
+	if y_Predict == 1:
+		result = "This is a phishing Site"
+	else:
+		result = "This is a legitimate Site"
+	return result
 
 
 if __name__ == "__main__":
